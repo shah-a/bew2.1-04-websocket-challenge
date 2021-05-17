e26c5f989168df0e9bf8a943bd39f54c9a5577fb
Ali Shah <s.ashah@outlook.com>
2021-05-17 01:37:19 -0400

    [Finished] some homework challenges
    Notes:
    - [Added] functionality for users connecting/disconnecting
      - Caveat: if client refreshes browser too quick, server won't have
        enough time to register the disconnection, and will register the
        reconnection as a new user coming online. The number of users
        will be shown to increase, rather than stay the same as before
        the client was refreshed
    - [Updated] message event emission to not be broadcasted to the
      sending client. A client's own messages will be appended to the
      chat via the client-side, not the server-side.

--- src/index.js
+++ src/index.js
@@ -13,11 +13,23 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
+let numUsers = 0;
+
 io.on('connection', (socket) => {
-  console.log('A user connected!');
+  // Broadcast when client joins the chat
+  numUsers++;
+  io.emit('user connected', numUsers);
+
+  // Broadcast when client leaves the chat
+  socket.on('disconnect', () => {
+    numUsers--;
+    socket.broadcast.emit('user disconnected', numUsers);
+  });
+
+  // Broadcast messages to connected sockets
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // <-- for emitting back to all sockets, including msg sender
-    // socket.broadcast.emit('chat message', msg); // <-- for emitting back to all sockets, except msg sender
+    // io.emit('chat message', msg); // <-- for emitting back to all sockets, including msg sender
+    socket.broadcast.emit('chat message', msg); // <-- for emitting back to all sockets, except msg sender
   });
 });
 
--- static/script.js
+++ static/script.js
@@ -7,13 +7,40 @@ const messages = document.getElementById('messages');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (input.value) {
+    // Emit client's message for the server to send to other clients
     socket.emit('chat message', input.value);
+
+    // Append client's message to the chat
+    const item = document.createElement('li');
+    item.textContent = input.value;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+
+    // Reset client's input field
     input.value = '';
   }
 });
 
+// Receive broadcast when new client joins the chat
+socket.on('user connected', (numUsers) => {
+  const item = document.createElement('li');
+  item.textContent = `Oh goodie :D Someone's here! Number of users: ${numUsers}`;
+  item.style.fontWeight = 'bold';
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+// Receive broadcast when a client leaves the chat
+socket.on('user disconnected', numUsers => {
+  const item = document.createElement('li');
+  item.textContent = `Oh no D: Someone left! Number of users: ${numUsers}`;
+  item.style.fontWeight = 'bold';
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+// Receive broadcast when other clients send messages
 socket.on('chat message', (msg) => {
-  console.log(msg);
   const item = document.createElement('li');
   item.textContent = msg;
   messages.appendChild(item);
