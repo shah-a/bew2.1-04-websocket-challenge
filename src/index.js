5fdb93234f1cade64ac794d91b937bd74f487513
Ali Shah <s.ashah@outlook.com>
2021-05-17 01:08:48 -0400

    [Added] event broadcasting back to socket clients
    Notes:
    - Tutorial is complete, except for the homework section

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected!');
   socket.on('chat message', (msg) => {
-    console.log('message:', msg);
+    io.emit('chat message', msg); // <-- for emitting back to all sockets, including msg sender
+    // socket.broadcast.emit('chat message', msg); // <-- for emitting back to all sockets, except msg sender
   });
 });
 
