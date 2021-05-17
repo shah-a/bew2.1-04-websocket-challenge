dd4b39ad4d65c28005b82bfc336b726550a58601
Ali Shah <s.ashah@outlook.com>
2021-05-17 00:45:31 -0400

    [Added] socket.io integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { join } = require('path');
+const express = require('express');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static(join(__dirname, '..', 'static')));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected!');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected!');
+  });
 });
 
 server.listen(3000, () => {
